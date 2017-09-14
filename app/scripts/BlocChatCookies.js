27278e5380d3a0336cfe41579e8d22cc987d8e12
Natalie Cook <natalie.t.cook@gmail.com>
2017-09-14 15:56:57 +0100

    ckpt 5 issues

---
+++ app/scripts/BlocChatCookies.js
@@ -0,0 +1,16 @@
+(function() {
+  function BlocChatCookies($cookies, $uibModal) {
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    if (!currentUser || currentUser === '') {
+      // Do something to allow users to set their username
+      $uibModal.open({
+        templateUrl: '/templates/login.html',
+        controller: 'LoginCtrl as login'
+      });
+    }
+  }
+
+  angular
+    .module('blocChat')
+    .run(['$cookies', '$uibModal', BlocChatCookies]);
+})();
